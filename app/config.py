7220c07a90fd5d8af44fd1132e4b55ef52b02635
Harisankar <harisankar@uniqnex360.com>
2026-01-13 13:00:25 +0530

    feature:GEMINI API KEY integration done

--- app/config.py
+++ app/config.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     openai_api_key:str
     llm_model:str='gpt-5'
+    gemini_api_key:str
+    gemini_model: str = "gemini-1.5-flash"
     enrichment_confidence_threashold:float=0.8
     hitl_confidence_threashold:float=0.85
     cloudinary_cloud_name:str 
--- app/llm.py
+++ app/llm.py
@@ -1,14 +1,26 @@
 from openai import OpenAI
 from .config import settings
+import google.generativeai as genai
 import json
+from app.sacred import generate_search_queries
 
 client = OpenAI(api_key=settings.openai_api_key)
-
-def call_llm(prompt: str, schema: dict) -> dict:
-    print(f"Using model: {settings.llm_model}")
-    print(f"API key exists: {bool(settings.openai_api_key)}")
+genai.configure(api_key=settings.gemini_api_key)
+def parse_response(content:str)->dict:
+    content=content.strip()
+    print(f"Raw content: {repr(content)}")
+    if content.startswith("```json"):
+        content = content[7:-3]
+    elif content.startswith("```"):
+        content = content[3:-3]
+    print(f"After stripping: {repr(content)}")
     
-    response = client.chat.completions.create(
+def call_llm(prompt: str, schema: dict) -> dict:
+    try:
+        print(f"Using model: {settings.llm_model}")
+        print(f"API key exists: {bool(settings.openai_api_key)}")
+    
+        response = client.chat.completions.create(
         model=settings.llm_model,
         messages=[
             {"role": "user", "content": prompt}
@@ -16,22 +28,17 @@ def call_llm(prompt: str, schema: dict) -> dict:
         response_format={"type": "json_object"},
         max_completion_tokens=4000
     )
-    
-    print(f"Full response: {response}")
-    print(f"Choices: {response.choices}")
-    print(f"First choice: {response.choices[0]}")
-    print(f"Message: {response.choices[0].message}")
-    print(f"Content: {response.choices[0].message.content}")
-    
-    content = response.choices[0].message.content.strip()
-    
-    print(f"Raw content: {repr(content)}")
-    
-    if content.startswith("```json"):
-        content = content[7:-3]
-    elif content.startswith("```"):
-        content = content[3:-3]
-    
-    print(f"After stripping: {repr(content)}")
-    
-    return json.loads(content)
+        print(f"Full response: {response}")
+        content = response.choices[0].message.content.strip()
+        return parse_response(content)
+    except Exception as e:
+        print(f"Open AI failed:{str(e)}")
+        print(f"---Switching  to Gemini backup ({settings.gemini_model})")
+        try:
+            model=genai.GenerativeModel(model_name=settings.gemini_model,generation_config={'response_mime_typ':'application/json'})
+            gemini_prompt=f'{prompt}\n\Return JSON response matching this schema:{json.dumps(schema)}'
+            response=model.generate_content(gemini_prompt)
+            return parse_response(response.text)
+        except Exception as e:
+            print(f"Gemini Backup also failed: {str(e)}")
+            return e
