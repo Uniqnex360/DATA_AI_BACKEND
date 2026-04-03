944d2131ee38ed36ba389f570e272bec5fed4b79
Harisankar <harisankar@uniqnex360.com>
2026-04-03 12:00:40 +0530

    fix:Changed claude model

--- app/core/config.py
+++ app/core/config.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
     MIGRATION_MODE: str = "safe" 
     gemini_api_key:str
     anthropic_api_key:str
-    claude_model: str = "claude-3-5-sonnet-20241022"
+    claude_model: str = "claude-sonnet-4-5"
     gemini_model: str = "gemini-2.5-flash"
     enrichment_confidence_threashold:float=0.8
     HITL_CONFIDENCE_THRESHOLD:float=0.85
