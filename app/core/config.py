accba139aaf48b9262537a9fbab45d44b1b09e62
Harisankar <harisankar@uniqnex360.com>
2026-09-15 18:08:34 +0530

    fix:Issues resolved

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = 'Data AI Backend'
     BACKEND_CORS_ORIGINS: List[str] = ["http://localhost:5173",
-                                       "http://localhost:3000", "https://data-ai-frontend-dusky.vercel.app"]
+                                       "http://localhost:3000", "https://data-ai-frontend-dusky.vercel.app","https://platform.datavioai.com"]
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     DATABASE_URL: str
